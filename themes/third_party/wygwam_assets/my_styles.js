5c2da09a21a1313b3b54094d028f9b7cd29e8699
Mike Gallagher <mail@mikegallagher.org>
2016-07-11 15:27:52 +0100

    revise home, all projects, and individual project pages
    plus a whole mess of image stuff. basically it is a full write-over

---
+++ themes/third_party/wygwam_assets/my_styles.js
@@ -0,0 +1,8 @@
+CKEDITOR.stylesSet.add( 'my_styles',
+[
+    // Block Styles
+    { name: 'Note', element: 'p', attributes: { 'class': 'note' } },
+    { name: 'Promo', element: 'p', attributes: { 'class': 'promo' } }
+
+
+]);
