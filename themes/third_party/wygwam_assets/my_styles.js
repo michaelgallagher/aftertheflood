16a6de8e0583491acbaefaa620ec04aaccc252c9
Mike Gallagher <mail@mikegallagher.org>
2017-08-14 17:16:15 +0100

    Begin adding the other Spark variations
    The line version is currently failing to decode. Seems like something is wrong with the font file itself. Explore.

--- themes/third_party/wygwam_assets/my_styles.js
+++ themes/third_party/wygwam_assets/my_styles.js
@@ -6,5 +6,8 @@ CKEDITOR.stylesSet.add( 'my_styles',
     { name: 'Large text, black', element: 'p', attributes: { 'class': 'large-text-black' } },
     { name: 'Large text, purple', element: 'p', attributes: { 'class': 'large-text-purple' } },
     { name: 'Spark bar - medium', element: 'span', attributes: { 'class': 'spark-bar-medium' } },
-
+    { name: 'Spark bar - narrow', element: 'span', attributes: { 'class': 'spark-bar-narrow' } },
+    { name: 'Spark bar - thin', element: 'span', attributes: { 'class': 'spark-bar-thin' } },
+    { name: 'Spark dot - medium', element: 'span', attributes: { 'class': 'spark-dot-medium' } },
+    { name: 'Spark line - medium', element: 'span', attributes: { 'class': 'spark-line-medium' } }
 ]);
