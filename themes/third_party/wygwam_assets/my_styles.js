c1da661463b8bfb07a7c072a6c663953c1580ccc
Mike Gallagher <mail@mikegallagher.org>
2017-08-14 13:18:33 +0100

    Spark support, part 2

--- themes/third_party/wygwam_assets/my_styles.js
+++ themes/third_party/wygwam_assets/my_styles.js
@@ -3,7 +3,8 @@ CKEDITOR.stylesSet.add( 'my_styles',
     // Block Styles
     { name: 'Note', element: 'p', attributes: { 'class': 'note' } },
     { name: 'Promo', element: 'p', attributes: { 'class': 'promo' } },
-    { name: 'Large text', element: 'p', attributes: { 'class': 'large-text' } },
+    { name: 'Large text, black', element: 'p', attributes: { 'class': 'large-text-black' } },
+    { name: 'Large text, purple', element: 'p', attributes: { 'class': 'large-text-purple' } },
     { name: 'Spark bar - medium', element: 'span', attributes: { 'class': 'spark-bar-medium' } },
 
 ]);
