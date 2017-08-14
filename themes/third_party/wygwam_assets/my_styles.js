b34e42fd40d3af26081add5dce14de252f14c72b
Mike Gallagher <mail@mikegallagher.org>
2017-08-14 10:38:19 +0100

    Add basic support for Spark font and large text illustrations

--- themes/third_party/wygwam_assets/my_styles.js
+++ themes/third_party/wygwam_assets/my_styles.js
@@ -2,7 +2,8 @@ CKEDITOR.stylesSet.add( 'my_styles',
 [
     // Block Styles
     { name: 'Note', element: 'p', attributes: { 'class': 'note' } },
-    { name: 'Promo', element: 'p', attributes: { 'class': 'promo' } }
-
+    { name: 'Promo', element: 'p', attributes: { 'class': 'promo' } },
+    { name: 'Large text', element: 'p', attributes: { 'class': 'large-text' } },
+    { name: 'Spark bar - medium', element: 'span', attributes: { 'class': 'spark-bar-medium' } },
 
 ]);
