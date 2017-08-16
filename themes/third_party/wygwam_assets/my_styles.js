7aad2cdd63c2937eea3fd3c36ca1b86d9705bbf9
Mike Gallagher <mail@mikegallagher.org>
2017-08-16 09:50:25 +0100

    Add hero image, revise fonts and example layout

--- themes/third_party/wygwam_assets/my_styles.js
+++ themes/third_party/wygwam_assets/my_styles.js
@@ -3,11 +3,14 @@ CKEDITOR.stylesSet.add( 'my_styles',
     // Block Styles
     { name: 'Note', element: 'p', attributes: { 'class': 'note' } },
     { name: 'Promo', element: 'p', attributes: { 'class': 'promo' } },
+    { name: 'Grey text', element: 'span', attributes: { 'class': 'grey-text' } },
+    { name: 'Text-illustration', element: 'p', attributes: { 'class': 'text-illustration' } },
     { name: 'Large text, black', element: 'p', attributes: { 'class': 'large-text-black' } },
     { name: 'Large text, purple', element: 'p', attributes: { 'class': 'large-text-purple' } },
     { name: 'Spark bar - medium', element: 'span', attributes: { 'class': 'spark-bar-medium' } },
     { name: 'Spark bar - narrow', element: 'span', attributes: { 'class': 'spark-bar-narrow' } },
     { name: 'Spark bar - thin', element: 'span', attributes: { 'class': 'spark-bar-thin' } },
     { name: 'Spark dot - medium', element: 'span', attributes: { 'class': 'spark-dot-medium' } },
+    { name: 'Spark dot - small', element: 'span', attributes: { 'class': 'spark-dot-small' } },
     { name: 'Spark line - medium', element: 'span', attributes: { 'class': 'spark-line-medium' } }
 ]);
